096e025bc4134281e0e9b88ef309af1f671dfb53
Joachim Løvgaard <joachim@loevgaard.dk>
2023-12-19 10:46:34 +0100

    Remove unused code

--- src/EventSubscriber/BeginCheckoutSubscriber.php
+++ src/EventSubscriber/BeginCheckoutSubscriber.php
@@ -6,22 +6,11 @@ namespace Setono\SyliusPlausiblePlugin\EventSubscriber;
 
 use Setono\SyliusPlausiblePlugin\Event\Plausible\Event;
 use Setono\SyliusPlausiblePlugin\Event\Plausible\Events;
-use Sylius\Component\Core\Model\OrderInterface;
-use Sylius\Component\Order\Context\CartContextInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\Messenger\MessageBusInterface;
-use Webmozart\Assert\Assert;
 
 final class BeginCheckoutSubscriber extends AbstractEventSubscriber
 {
-    public function __construct(
-        MessageBusInterface $eventBus,
-        private readonly CartContextInterface $cartContext,
-    ) {
-        parent::__construct($eventBus);
-    }
-
     public static function getSubscribedEvents(): array
     {
         return [
@@ -35,16 +24,11 @@ final class BeginCheckoutSubscriber extends AbstractEventSubscriber
             if (!$requestEvent->isMainRequest()) {
                 return;
             }
-            $request = $requestEvent->getRequest();
 
-            $route = $request->attributes->get('_route');
+            $route = $requestEvent->getRequest()->attributes->get('_route');
             if ('sylius_shop_checkout_start' !== $route) {
                 return;
             }
-
-            /** @var OrderInterface $order */
-            $order = $this->cartContext->getCart();
-            Assert::isInstanceOf($order, OrderInterface::class);
 
             $this->eventBus->dispatch(new Event(Events::BEGIN_CHECKOUT));
         } catch (\Throwable $e) {
