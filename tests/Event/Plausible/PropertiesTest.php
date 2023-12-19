43174c5a915ece64d1b9581eee7b5d23fcdf19c1
Joachim Løvgaard <joachim@loevgaard.dk>
2023-12-19 10:52:10 +0100

    Add more tests

--- tests/Event/Plausible/PropertiesTest.php
+++ tests/Event/Plausible/PropertiesTest.php
@@ -26,4 +26,44 @@ final class PropertiesTest extends TestCase
             'property1' => 'value',
         ], $properties->jsonSerialize());
     }
+
+    /**
+     * @test
+     */
+    public function it_is_countable(): void
+    {
+        $properties = new Properties();
+        self::assertCount(0, $properties);
+
+        $properties->set('property1', 'value');
+        self::assertCount(1, $properties);
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_iterable(): void
+    {
+        $properties = new Properties();
+        self::assertSame([], iterator_to_array($properties));
+
+        $properties->set('property1', 'value');
+        self::assertSame(['property1' => 'value'], iterator_to_array($properties));
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_array_accessible(): void
+    {
+        $properties = new Properties();
+        self::assertFalse(isset($properties['property1']));
+
+        $properties['property1'] = 'value';
+        self::assertTrue(isset($properties['property1']));
+        self::assertSame('value', $properties['property1']);
+
+        unset($properties['property1']);
+        self::assertFalse(isset($properties['property1']));
+    }
 }
