007f7631825e1181ac56d73be200fdb019da6d40
Joachim Løvgaard <joachim@loevgaard.dk>
2025-04-28 09:30:50 +0200

    Fix some false positives

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,9 @@
 <?php
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('setono/tag-bag-bundle', [ErrorType::UNUSED_DEPENDENCY])
 ;
